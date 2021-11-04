5ef47ba5dea6c7c789bae0b3d32384d133cf92f1
PabloSanchezB <pabloasb87@gmail.com>
2021-11-03 20:04:26 -0500

    Contest

--- Ejercicio_React_15_10_2021/store_app/src/componentes/CheckoutBox.js
+++ Ejercicio_React_15_10_2021/store_app/src/componentes/CheckoutBox.js
@@ -5,6 +5,8 @@ export default function CheckoutBox(){
     const {clearCart, handleCheckout, itemCount, total} = useContext(CartContext);
     
     return(
-        <></>
+        <>
+        
+        </>
     );
 }
